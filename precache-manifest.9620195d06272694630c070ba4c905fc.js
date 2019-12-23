788aabbe0ad2f9fd33d462b6dc6eef33b71ecda2
Herman Wika Horn <herman.wika.horn@entur.org>
2019-12-24 00:39:24 +0100

    Updates

---
+++ precache-manifest.9620195d06272694630c070ba4c905fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad33abd62bf760a6f655995c1b3c861",
+    "url": "/ntnu-course-picker/index.html"
+  },
+  {
+    "revision": "61d416595d7d182cbc0a",
+    "url": "/ntnu-course-picker/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bfb70290efdf4176a92c",
+    "url": "/ntnu-course-picker/static/css/main.aa2b2684.chunk.css"
+  },
+  {
+    "revision": "61d416595d7d182cbc0a",
+    "url": "/ntnu-course-picker/static/js/2.14027dd3.chunk.js"
+  },
+  {
+    "revision": "540146be997c2634c85ae11458dc90be",
+    "url": "/ntnu-course-picker/static/js/2.14027dd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfb70290efdf4176a92c",
+    "url": "/ntnu-course-picker/static/js/main.4bb52f9d.chunk.js"
+  },
+  {
+    "revision": "27fbc326b1b10c425858",
+    "url": "/ntnu-course-picker/static/js/runtime-main.728df32f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ntnu-course-picker/static/media/logo.5d5d9eef.svg"
+  }
+]);
